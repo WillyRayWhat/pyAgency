2f061b2343ad192cfc4c5c4572d8b24f16fa9056
Willy Ray <willyray@gmail.com>
2021-09-19 16:14:48 -0600

    Add some structure

---
+++ tests/test_stub.py
@@ -0,0 +1,10 @@
+"""
+test stub so that pre-commit hooks will have
+sopmething to do.
+"""
+
+
+def test_answer():
+    """test so that pre-commit hooks will have something
+    to do."""
+    assert True
